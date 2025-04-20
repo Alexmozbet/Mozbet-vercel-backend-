d9b3f581b167457eaaf5cf2f76c4f51f9f29a33d
Alexmozbet <alexmariojonas@gmail.com>
2025-04-20 10:29:52 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 export default function handler(req, res) {
   res.status(200).json({
-    message: "Backend MozBet online com sucesso!",
-    autor: "Alex Macuvel",
-    status: "ok"
+    message: "MozBet backend funcionando!",
+    autor: "Alex Macuvel"
   });
 }
