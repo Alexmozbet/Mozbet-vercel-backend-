10f3221500fc822a93e1b660aad2df310cc95976
Alexmozbet <alexmariojonas@gmail.com>
2025-04-20 11:43:59 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    message: "MozBet backend funcionando!",
-    autor: "Alex Macuvel"
-  });
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).json({
+      status: "online",
+      projeto: "MozBet",
+      backend: "Operacional",
+      autor: "Alex Macuvel",
+      mensagem: "MozBet Backend Online com Sucesso!"
+    });
+  } else {
+    res.status(405).json({ erro: "Método não permitido" });
+  }
 }
